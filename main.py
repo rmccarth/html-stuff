294fe26a2c3875fcfc231ab7eaaee3b8e29ce319
Robert McCarthy <remccarthy@sei.cmu.edu>
2020-08-09 20:53:23 -0400

    regex parsing

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import requests
 from bs4 import BeautifulSoup as bs4
+import json
+import re
 
 main_page = "https://www.youtube.com/feed/trending"
 
 response = requests.get(main_page)
-soup = bs4(response.text, features='lxml')
-links = soup.findAll('a',attrs={'class':'yt-uix-tile-link'})
+soup = bs4(response.text, 'html.parser')
+scripts = soup.find_all('script')
 
-print(links)
+for script in scripts:
+    
+    urls = re.findall(r"/watch(.*?)[\"]", str(script))
+    if len(urls) > 1:
+        for url in urls:
+            url = url.split("\\n")[0]
+            url = "https://www.youtube.com/watch" + url
+            print(url)
+
 
